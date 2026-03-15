d2616c3065c739502f3b04a4203b21b9f825aeec
Eltarchik <makarteni@gmail.com>
2026-03-15 17:51:53 +0300

    toggle theme animation fix

--- src/shared/config/theme.ts
+++ src/shared/config/theme.ts
@@ -1,5 +1,14 @@
 export const LIGHT_THEME = "light"
 
+let timeout: NodeJS.Timeout | null = null
+
 export const toggleTheme = () => {
+    document.documentElement.classList.add("theme-transition")
     document.documentElement.classList.toggle(LIGHT_THEME)
+
+    if (timeout) clearTimeout(timeout)
+
+    timeout = setTimeout(() => {
+        document.documentElement.classList.remove("theme-transition")
+    }, 80)
 }
